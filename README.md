f67c35ef9927641262328c545f87c9425ebd04e2
madJhon <mzlenko25@gmail.com>
2023-08-18 13:30:55 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# peditor-test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/hmksut)
---
+++ src/app/demo/editor-basic-demo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'editor-basic-demo',
+  templateUrl: './editor-basic-demo.html',
+})
+export class EditorBasicDemo {
+  text: string | undefined;
+}
